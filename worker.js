5b746e2b4e5825a82892e50eb6f9f517c1dd9351
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-01-11 14:59:00 +0200

    stuff

--- worker.js
+++ worker.js
@@ -3,3 +3,6 @@
 // Web workers are used to run long-running scripts in the background without blocking the main thread. The main thread is responsible for updating the UI and handling user interactions. If a long-running script is executed on the main thread, it will block the UI and the user won't be able to interact with the page until the script is finished. Web workers are used to run long-running scripts in the background without blocking the main thread.
 // Note: Web workers don't have access to the DOM, window object, or global variables. They run in a separate thread and don't have access to the main thread. They communicate with the main thread using the postMessage() method.
 // In Node JS, we can use the worker_threads module to create web workers. The worker_threads module provides a Worker class that can be used to create web workers. The Worker class takes the path to the worker script as an argument and creates a new web worker that runs the script in a separate thread.
+const {spawn} = require('child_process');
+
+spawn('git', ['stuff'])
